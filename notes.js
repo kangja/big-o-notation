279aea6c3111dab860c9ac29fb333a9d47a5ecc6
hi <kangja1996@gmail.com>
2020-10-17 11:51:15 -0400

    progress

--- notes.js
+++ notes.js
@@ -22,7 +22,35 @@ for (let i = 0; i < data.length; i++) {
 
 //how our algorithm grows as n grows. as n goes to 3 to 300, we run the loop from 3 times to 300 times.
 
+// Example 1's big o notation is O(n). As our input scales from 3 to 300, our algorithm is going to scale in time linearly. It grows 1 to 1 with the size of our input. 
+// ****************************** 
 
 
-// ******************************
+
+
+
+
+// EXAMPLE 2 ******************************
+const data = ["A", "B", "C"];
+const data2 = [1,2,3,4,5]
+
+for (let i = 0; i < data.length; i++) {
+  console.log(data[i])
+}
+
+for (let j = 0; j < data2.length; j++) {
+  console.log(data2[j]);
+}
+
+
+//n represents data and a represents the length of data2. 
+//So our algorithm is going to scale based on the length of data1 and data2. We have a big o of O(n+ a)
+// we loop through everything in data2(which is a) and we loop through everything in data1(n times) because it's the length of of data1. We have n + a length loop here. 
+// ****************************** 
+
+
+
+
+
+// EXAMPLE 3 ******************************
 
